cdebae65341c7829f7f9349f4298e7a8d06d2a84
gergely.m <m.gergely1999@gmail.com>
2024-02-10 13:57:00 +0100

    ♻️ move MagmaVizClient to new repository

---
+++ client/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MagmaVizClient',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    # Metadata
+    author='Gergely Magyar',
+    author_email='m.gergely1999@gmail.com',
+    description='Used to send data to the MagmaViz server via UDP',
+    url='https://github.com/MagmaMultiAgent/MagmaViz',
+    license='MIT',
+)
